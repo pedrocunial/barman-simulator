539ad705cda61ea022df82d922014f189a0e2899
Pedro Cunial <pedrocc4@al.insper.edu.br>
2017-10-10 08:29:38 -0300

    wip

--- Assets/scripts/ThrowObjectWithScore.cs
+++ Assets/scripts/ThrowObjectWithScore.cs
@@ -3,26 +3,29 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
-public class ThrowObjectWithScore : MonoBehaviour {
+public class ThrowObjectWithScore : MonoBehaviour
+{
 
 	public AudioClip whoaSound;
 
 	public UnityEvent OnPickUp;
-	public UnityEvent On
 
 	// Use this for initialization
-	void Start () {}
+	void Start ()
+	{
+		
+	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+	{
 		
 	}
 
-	public void PickUpEvent(){
+	public void PickUpEvent ()
+	{
 		OnPickUp.Invoke ();
 		AudioSource.PlayClipAtPoint (whoaSound, transform.position);
 	}
-
-
 
 }
