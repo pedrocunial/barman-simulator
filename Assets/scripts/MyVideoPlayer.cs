b8a18d9d789705f50de110aaa694fcf58f1ab4a5
Pedro Cunial <pedrocc4@al.insper.edu.br>
2017-10-10 09:07:30 -0300

    tv model

---
+++ Assets/scripts/MyVideoPlayer.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UnityEngine.Video;
+
+//[RequireComponent (typeof(VideoClip))]
+//[RequireComponent (typeof(AudioSource))]
+//[RequireComponent (typeof(Texture2D[]))]
+public class MyVideoPlayer : MonoBehaviour
+{
+	private int currentImage = 0;
+	private double timePassed = 0.0f;
+	private Renderer renderer;
+
+	public float delta = 0.2f;
+	public Texture2D[] frames;
+	public String shaderName;
+	public String textureName;
+	public AudioClip music;
+	public MovieTexture videoClip;
+
+
+	// Use this for initialization
+	void Start ()
+	{
+//		updateFrame ();
+		((MovieTexture)GetComponent <Renderer> ().material.mainTexture).Play ();
+		AudioSource.PlayClipAtPoint (music, transform.position);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+//		timePassed += Time.deltaTime;
+//		if (timePassed > delta) {
+//			timePassed = 0.0f;
+//			updateFrame ();
+//		}
+	}
+
+	private void updateFrame ()
+	{
+//		int i = currentImage % frames.Length;
+//		renderer.material.SetTexture (textureName, frames [i]);
+//		currentImage++;
+	}
+}
--- Assets/scripts/ThrowObjectWithScore.cs
+++ Assets/scripts/ThrowObjectWithScore.cs
@@ -2,30 +2,144 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using Valve.VR.InteractionSystem;
+using System.Runtime.ConstrainedExecution;
 
+[RequireComponent (typeof(Throwable))]
+[RequireComponent (typeof(Interactable))]
+[RequireComponent (typeof(TextMesh))]
 public class ThrowObjectWithScore : MonoBehaviour
 {
 
+	private const int TIME_MULTIPLIER = 10000;
+
 	public AudioClip whoaSound;
 
-	public UnityEvent OnPickUp;
+	private TextMesh textMesh;
+	private Vector3 oldPosition;
+	private Quaternion oldRotation;
 
-	// Use this for initialization
-	void Start ()
+	private float attachTime;
+	private float throwTime;
+
+	private bool flying;
+
+	private Hand.AttachmentFlags attachmentFlags =
+		Hand.defaultAttachmentFlags
+		& (~Hand.AttachmentFlags.SnapOnAttach)
+		& (~Hand.AttachmentFlags.DetachOthers);
+
+	//-------------------------------------------------
+	void Awake ()
 	{
-		
-	}
-	
-	// Update is called once per frame
-	void Update ()
-	{
-		
+		flying = false;
+		textMesh = GetComponentInChildren<TextMesh> ();
+		textMesh.text = "No Hand Hovering";
 	}
 
-	public void PickUpEvent ()
+
+	void OnCollisionEnter (Collision col)
 	{
-		OnPickUp.Invoke ();
-		AudioSource.PlayClipAtPoint (whoaSound, transform.position);
+		flying = false;
 	}
 
+	//-------------------------------------------------
+	// Called when a Hand starts hovering over this object
+	//-------------------------------------------------
+	private void OnHandHoverBegin (Hand hand)
+	{
+//		textMesh.text = "Hovering hand: " + hand.name;
+	}
+
+
+	//-------------------------------------------------
+	// Called when a Hand stops hovering over this object
+	//-------------------------------------------------
+	private void OnHandHoverEnd (Hand hand)
+	{
+//		textMesh.text = "No Hand Hovering";
+	}
+
+
+	//-------------------------------------------------
+	// Called every Update() while a Hand is hovering over this object
+	//-------------------------------------------------
+	private void HandHoverUpdate (Hand hand)
+	{
+//		if (hand.GetStandardInteractionButtonDown () || ((hand.controller != null) && hand.controller.GetPressDown (Valve.VR.EVRButtonId.k_EButton_Grip))) {
+//			if (hand.currentAttachedObject != gameObject) {
+//				// Save our position/rotation so that we can restore it when we detach
+//				oldPosition = transform.position;
+//				oldRotation = transform.rotation;
+//
+//				// Call this to continue receiving HandHoverUpdate messages,
+//				// and prevent the hand from hovering over anything else
+//				hand.HoverLock (GetComponent<Interactable> ());
+//
+//				// Attach this object to the hand
+//				hand.AttachObject (gameObject, attachmentFlags);
+//			} else {
+//				// Detach this object from the hand
+//				hand.DetachObject (gameObject);
+//
+//				// Call this to undo HoverLock
+//				hand.HoverUnlock (GetComponent<Interactable> ());
+//
+//				// Restore position/rotation
+//				transform.position = oldPosition;
+//				transform.rotation = oldRotation;
+//			}
+//		}
+	}
+
+
+	//-------------------------------------------------
+	// Called when this GameObject becomes attached to the hand
+	//-------------------------------------------------
+	private void OnAttachedToHand (Hand hand)
+	{
+		if (flying) {
+			// regrab
+			textMesh.text = "+" + (TIME_MULTIPLIER * (Time.time - throwTime)).ToString ("F0");
+			flying = false;
+		} 
+	}
+
+
+	//-------------------------------------------------
+	// Called when this GameObject is detached from the hand
+	//-------------------------------------------------
+	private void OnDetachedFromHand (Hand hand)
+	{
+		throwTime = Time.time;
+		textMesh.text = "WOOOOOOW! Thrown at: " + this.throwTime;
+		flying = true;
+		AudioSource.PlayClipAtPoint (this.whoaSound, transform.position);
+		//textMesh.text = "Detached from hand: " + hand.name;
+	}
+
+
+	//-------------------------------------------------
+	// Called every Update() while this GameObject is attached to the hand
+	//-------------------------------------------------
+	private void HandAttachedUpdate (Hand hand)
+	{
+		//textMesh.text = "Attached to hand: " + hand.name + "\nAttached time: " + (Time.time - attachTime).ToString ("F2");
+	}
+
+
+	//-------------------------------------------------
+	// Called when this attached GameObject becomes the primary attached object
+	//-------------------------------------------------
+	private void OnHandFocusAcquired (Hand hand)
+	{
+	}
+
+
+	//-------------------------------------------------
+	// Called when another attached GameObject becomes the primary attached object
+	//-------------------------------------------------
+	private void OnHandFocusLost (Hand hand)
+	{
+	}
 }
