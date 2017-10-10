c58f9fb42009642e3a825973f8dc2d293a1260b9
Pedro Cunial <pedrocc4@al.insper.edu.br>
2017-10-10 09:48:13 -0300

    tv done

--- Assets/scripts/MyVideoPlayer.cs
+++ Assets/scripts/MyVideoPlayer.cs
@@ -7,26 +7,36 @@ using UnityEngine.Video;
 //[RequireComponent (typeof(VideoClip))]
 //[RequireComponent (typeof(AudioSource))]
 //[RequireComponent (typeof(Texture2D[]))]
+using UnityEngine.Experimental.UIElements;
+using UnityEngine.UI;
+
+
 public class MyVideoPlayer : MonoBehaviour
 {
 	private int currentImage = 0;
 	private double timePassed = 0.0f;
 	private Renderer renderer;
 
+	public RawImage image;
+
 	public float delta = 0.2f;
 	public Texture2D[] frames;
 	public String shaderName;
 	public String textureName;
 	public AudioClip music;
-	public MovieTexture videoClip;
+
+	private VideoPlayer videoPlayer;
+	private VideoSource videoSource;
+	private AudioSource audioSource;
+	public VideoClip videoClip;
 
 
 	// Use this for initialization
 	void Start ()
 	{
 //		updateFrame ();
-		((MovieTexture)GetComponent <Renderer> ().material.mainTexture).Play ();
-		AudioSource.PlayClipAtPoint (music, transform.position);
+//		AudioSource.PlayClipAtPoint (music, transform.position);
+		StartCoroutine (playVideo ());
 	}
 	
 	// Update is called once per frame
@@ -39,10 +49,36 @@ public class MyVideoPlayer : MonoBehaviour
 //		}
 	}
 
-	private void updateFrame ()
+	private IEnumerator playVideo ()
 	{
-//		int i = currentImage % frames.Length;
-//		renderer.material.SetTexture (textureName, frames [i]);
-//		currentImage++;
+		videoPlayer = gameObject.AddComponent<VideoPlayer> ();
+		audioSource = gameObject.AddComponent<AudioSource> ();
+
+		videoPlayer.playOnAwake = false;
+		audioSource.playOnAwake = false;
+		audioSource.Pause ();
+
+		videoPlayer.source = VideoSource.VideoClip;
+
+		videoPlayer.audioOutputMode = VideoAudioOutputMode.AudioSource;
+
+		videoPlayer.EnableAudioTrack (0, true);
+		videoPlayer.SetTargetAudioSource (0, audioSource);
+
+		videoPlayer.clip = videoClip;
+		videoPlayer.Prepare ();
+
+		// wait while video is prepared
+		WaitForSeconds waitTime = new WaitForSeconds (1);
+		while (!videoPlayer.isPrepared) {
+			Debug.Log ("Preparing");
+			yield return waitTime;
+			break;
+		}
+
+		image.texture = videoPlayer.texture;
+
+		videoPlayer.Play ();
+		audioSource.Play ();
 	}
 }
