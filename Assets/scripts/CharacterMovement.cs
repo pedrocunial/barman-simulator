e25aa68fbf68aa222e32f327a2964c0e2ddd4c16
Pedro Cunial <pedrocc4@al.insper.edu.br>
2017-10-17 08:42:34 -0200

    my friend ethan

---
+++ Assets/scripts/CharacterMovement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+
+	public float speed;
+	public float maxDistance;
+
+	public GameObject finalObjective;
+
+
+	// Use this for initialization
+	void Start ()
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+
+		float distance = Vector3.Distance (transform.position,
+			                 finalObjective.transform.position);
+		distance = distance > 0 ? distance : -distance;  // abs
+
+//		if (distance > maxDistance)
+		// Only move when not at the bar
+		transform.position = new Vector3 (
+			transform.position.x,
+			transform.position.y,
+			transform.position.z + speed
+		);
+
+	}
+}
